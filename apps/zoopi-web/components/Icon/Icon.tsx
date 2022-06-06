f8fb9846b72048245aae2bf180adb1ffc1031fab
jaehyeon(wiz) <eiirwp@gmail.com>
2022-06-06 19:21:24 +0900

    add icon component

---
+++ apps/zoopi-web/components/Icon/Icon.tsx
@@ -0,0 +1,36 @@
+import {useTheme} from "@emotion/react"
+import React from "react"
+import { ReactSVG } from 'react-svg'
+
+type IconName = |
+  "arrow-back" |
+  "arrow-right" |
+  "check-circle" |
+  "circle" |
+  "close-circle" |
+  "close" |
+  "eye" |
+  "search"
+
+export type IconProps = {
+  name: IconName
+  color?: string
+  size?: number
+}
+
+export const Icon = ({
+  color,
+  size = 24,
+  name,
+}: IconProps) => {
+  const theme = useTheme()
+  
+  return (
+    <span css={{
+      color: color ? color : theme.colors["grey-90"],
+      fontSize: 0,
+    }}>
+      <ReactSVG src={`icons/${name}.svg`} width={size} height={size}/>
+    </span>
+  )
+}
--- apps/zoopi-web/pages/index.tsx
+++ apps/zoopi-web/pages/index.tsx
@@ -1,13 +1,17 @@
 import {useTheme} from "@emotion/react"
+import { Icon } from "@web/components/Icon"
 
 const HomePage = () => {
   const theme = useTheme()
 
   return (
-    <div css={{
-      color: theme.colors.main
-    }}>
-      Home
+    <div>
+      <div css={{
+        color: theme.colors.main
+      }}>
+        Home
+      </div>
+      <Icon name={"circle"}/>
     </div>
   )
 }
