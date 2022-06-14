5bf898abfd2ed8d1c95fa7c0fe80d3380e139a1c
hotbreakb <areyhappy12@gmail.com>
2022-06-14 21:04:13 +0900

    add AnimalChip in index

--- apps/zoopi-web/pages/index.tsx
+++ apps/zoopi-web/pages/index.tsx
@@ -1,19 +1,28 @@
-import {useTheme} from "@emotion/react"
-import { Icon } from "@web/components/Icon"
+import { useTheme } from '@emotion/react';
+import { AnimalChip } from '@web/components/AnimalChip';
+import { Icon } from '@web/components/Icon';
 
 const HomePage = () => {
-  const theme = useTheme()
+  const theme = useTheme();
 
   return (
     <div>
-      <div css={{
-        color: theme.colors.main
-      }}>
+      <div
+        css={{
+          color: theme.colors.main,
+        }}
+      >
         Home
       </div>
-      <Icon name={"circle"}/>
+      <Icon name={'circle'} />
+      <AnimalChip
+        isSelected={false}
+        name="코코"
+        type="dog"
+        bloodType={['DEA1-', 'DEA1.1']}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
