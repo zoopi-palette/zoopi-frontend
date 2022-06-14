a46a922d85076f8e2b523ee8411402d2bbadeeb0
hotbreakb <areyhappy12@gmail.com>
2022-06-14 16:19:18 +0900

    add select component

---
+++ apps/zoopi-web/components/Select/Select.stories.tsx
@@ -0,0 +1,16 @@
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import {Select} from "./Select";
+
+export default {
+  title: "atoms/Select",
+  component: Select,
+  argTypes: {},
+  args: {
+    id: "id",
+    name: "name",
+  },
+} as ComponentMeta<typeof Select>;
+
+const Template: ComponentStory<typeof Select> = (args) => <Select {...args} />;
+
+export const Default = Template.bind({});
---
+++ apps/zoopi-web/components/Select/Select.tsx
@@ -0,0 +1,43 @@
+import {ChangeEventHandler, ReactNode, useCallback} from "react";
+
+export type SelectProps = {
+  children?: ReactNode;
+  id: string;
+  name: string;
+  onChange?: ChangeEventHandler<HTMLSelectElement>;
+};
+
+export const Select = ({
+  children,
+  id,
+  name,
+  onChange,
+  ...rest
+}: SelectProps) => {
+  return (
+    <div
+      css={{
+        borderWidth: 1,
+        borderStyle: "solid",
+        borderRadius: "22px",
+        borderColor: "#E6E7E9",
+        paddingTop: 12,
+        paddingRight: 11,
+        paddingBottom: 13,
+        paddingLeft: 12,
+        marginLeft: 8,
+        marginRight: 8,
+      }}
+    >
+      <select
+        id={id}
+        onChange={onChange}
+        {...rest}
+        css={{border: 0, color: "#393939", fontSize: 13, paddingRight: 4}}
+      >
+        {children}
+        <option value="">{name}</option>
+      </select>
+    </div>
+  );
+};
