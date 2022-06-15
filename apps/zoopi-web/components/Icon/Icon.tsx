9a8939a9c74aebc0da34fa9079ac226fe2cb0788
hotbreakb <areyhappy12@gmail.com>
2022-06-15 13:34:55 +0900

    add plus-rectangle icon

--- apps/zoopi-web/components/Icon/Icon.tsx
+++ apps/zoopi-web/components/Icon/Icon.tsx
@@ -1,42 +1,42 @@
-import {useTheme} from "@emotion/react"
-import React from "react"
-import { ReactSVG } from 'react-svg'
+import { useTheme } from '@emotion/react';
+import React from 'react';
+import { ReactSVG } from 'react-svg';
 
-export type IconName = |
-  "arrow-back" |
-  "arrow-right" |
-  "check-circle" |
-  "circle" |
-  "close-circle" |
-  "close" |
-  "eye" |
-  "search"
+export type IconName =
+  | 'arrow-back'
+  | 'arrow-right'
+  | 'check-circle'
+  | 'circle'
+  | 'close-circle'
+  | 'close'
+  | 'eye'
+  | 'search'
+  | 'plus-rectangle';
 
 export type IconProps = {
-  name: IconName
-  color?: string
-  size?: number
-}
+  name: IconName;
+  color?: string;
+  size?: number;
+};
 
-export const Icon = ({
-  color,
-  size = 24,
-  name,
-}: IconProps) => {
-  const theme = useTheme()
-  
+export const Icon = ({ color, size = 24, name }: IconProps) => {
+  const theme = useTheme();
+
   return (
-    <span css={{
-      color: color ? color : theme.colors["grey-90"],
-      fontSize: 0,
-    }}>
-      <ReactSVG 
+    <span
+      css={{
+        color: color ? color : theme.colors['grey-90'],
+        fontSize: 0,
+      }}
+    >
+      <ReactSVG
         src={`icons/${name}.svg`}
         beforeInjection={(svg) => {
-          svg.setAttribute('style', `width: ${size}px; height: ${size}px;`)
+          svg.setAttribute('style', `width: ${size}px; height: ${size}px;`);
         }}
-        width={size} height={size}
-        />
+        width={size}
+        height={size}
+      />
     </span>
-  )
-}
+  );
+};
