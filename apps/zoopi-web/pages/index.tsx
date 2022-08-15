7a7f9ba3a7a75d6414c0b1cc41ca27d0ce10ded5
hotbreakb <areyhappy12@gmail.com>
2022-08-15 20:31:52 +0900

    Feat: onSelect

--- apps/zoopi-web/pages/index.tsx
+++ apps/zoopi-web/pages/index.tsx
@@ -1,9 +1,11 @@
 import { useTheme } from '@emotion/react';
 import { AnimalChip } from '@web/components/AnimalChip';
 import { Icon } from '@web/components/Icon';
+import { useState } from 'react';
 
 const HomePage = () => {
   const theme = useTheme();
+  const [isAnimalChipSelected, setIsAnimalChipSelected] = useState(false);
 
   return (
     <div>
@@ -16,10 +18,12 @@ const HomePage = () => {
       </div>
       <Icon name="circle" />
       <AnimalChip
-        isSelected={false}
+        isSelected={isAnimalChipSelected}
         animalName="코코"
         animalType="dog"
         bloodType={['DEA1-', 'DEA1.1']}
+        avatar="/images/ex-dog-animalchip.png"
+        onClick={() => setIsAnimalChipSelected(!isAnimalChipSelected)}
       />
     </div>
   );
